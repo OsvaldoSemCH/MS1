e6d2475fd660d4d3b0eb785f73d12c9389a9d68a
OsvaldoSemCH <eduardohenriqueribeirobrasil@gmail.com>
2024-08-28 15:40:48 -0300

    Cards básicos criados em Showroom/showroom.html

---
+++ Showroom/script.js
@@ -0,0 +1,58 @@
+document.addEventListener
+(
+    "DOMContentLoaded",
+    ()=>
+    {
+        fetch("../Dados/loja.json")
+        .then((response) => response.json())
+        .then((data) =>
+        {
+            const Container = document.getElementById("ShowroomContainer");
+            data.map((produto, index) =>
+            {
+                const card = document.createElement("div");
+                card.className = "card";
+                card.style.width = "18rem";
+                card.style.marginRight = "10px";
+                card.style.padding = "5px";
+                card.style.float = "left";
+
+                const imagem = document.createElement("img");
+                imagem.src = produto.imagem;
+                imagem.style.width = "100%";
+                imagem.style.aspectRatio = 1;
+                imagem.style.objectFit = "contain";
+
+                const cardBody = document.createElement("div");
+
+                const cardTitle = document.createElement("h3");
+                cardTitle.textContent = produto.descricao;
+
+                const cardText = document.createElement("p");
+                cardText.textContent = "Preço: $" + produto.preco.toFixed(2);
+
+                const circulo = document.createElement("div");
+                if(produto.status)
+                {
+                    circulo.style.backgroundColor = "#c00000";
+                }else
+                {
+                    circulo.style.backgroundColor = "#00c000";
+                }
+                circulo.style.width = "30px";
+                circulo.style.height = "30px";
+                circulo.style.margin = "3px";
+                circulo.style.borderRadius = "50%";
+
+                cardBody.appendChild(cardTitle);
+                cardBody.appendChild(cardText);
+                
+                card.appendChild(circulo);
+                card.appendChild(imagem);
+                card.appendChild(cardBody);
+        
+                Container.appendChild(card);
+            })
+        })
+    }
+)
